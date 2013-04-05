2028bd1f7744cf0542c0ad1cc94b485abd3f53f9
Artur Trofimov <arturtr@gmail.com>
2013-04-05 16:18:07 +1100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tehnoles3::Application.config.secret_token = 'c286221031c37041e467be6db5a6f61fbf09ab0ef981f418555fa2b4c20ce6042feb2a8869c2f878f27aecd8bd961fee96d20513ad4245e4b0ae42413b5e04b2'
