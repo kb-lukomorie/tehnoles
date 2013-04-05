5b5a71e5d04858ab2e0d5f2279dd5fd478f1a793
Artur Trofimov <arturtr@gmail.com>
2013-04-05 19:01:19 +1100

    fix assets cfg

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,8 +46,7 @@ Tehnoles3::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
-  config.assets.precompile += Ckeditor.assets
+  config.assets.precompile += %w( ckeditor/init.js ckeditor/ckeditor.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
